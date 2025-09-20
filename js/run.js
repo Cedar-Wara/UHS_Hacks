8bd4d26d702914cdc01dddf0c37efb33c9223720
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-09-20 14:04:42 -0700

    console log?

--- js/run.js
+++ js/run.js
@@ -1,16 +1,26 @@
-
-
 let frame = document.querySelector('.exec-frame iframe');
 
-document.querySelector(".run-button").addEventListener("click",()=>{
-    frame.contentDocument.location.reload(true);
+document.querySelector(".run-button").addEventListener("click", () => {
+  frame.contentDocument.location.reload(true);
 })
 
-frame.onload = ()=>{
-    console.log(getEditorValue())
-    frame.contentWindow.postMessage(getEditorValue())
+frame.onload = () => {
+  console.log(getEditorValue())
+  frame.contentWindow.postMessage(getEditorValue())
 }
 
 window.addEventListener('message', e => {
-    console.log(e)
+  // console.log(e)
+  logConsole(e.data.message, e.data.type)
 })
+
+function logConsole(output, type) {
+  let newEl = document.createElement("div")
+  newEl.innerHTML = `
+<div class="console-log ${type}">
+    <div class="output">${output}</div>
+</div>
+    `
+  document.querySelector(".console").appendChild(newEl)
+}
+
